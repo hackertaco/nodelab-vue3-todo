8415594b677eca5e9f6d2a79cf5c12c2bf95f6a9
hackertaco <imsa1224@naver.com>
2020-10-12 02:19:16 +0900

    feat: ⚡️show list

--- src/components/TodoLists.vue
+++ src/components/TodoLists.vue
@@ -1,1 +1,27 @@
+<template>
+<div class="lists-block">
+    <ul>
+        <li v-for="(todoItem, index) in todoItems" :key="todoItem.item"></li>
+    </ul>
+</div>
+</template>
 
+<script>
+export default {
+    data: function () {
+        return {
+            todoItems: [],
+        };
+    },
+    methods: {},
+};
+</script>
+
+<style>
+.lists-block {
+    flex: 1;
+    width: 100%;
+    height: 70%;
+    display: flex;
+}
+</style>
