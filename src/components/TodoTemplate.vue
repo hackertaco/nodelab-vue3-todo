6d387306b7bda7f7a205628ca2e55232e4c762fc
hackertaco <imsa1224@naver.com>
2020-10-12 02:19:36 +0900

    feat: ⚡️ basic template

--- src/components/TodoTemplate.vue
+++ src/components/TodoTemplate.vue
@@ -1,16 +1,20 @@
 <template>
 <div class="todo-background">
-    <h2>TODOLIST</h2>
-    <TodoInput />
+    <todo-header></todo-header>
+    <todo-input></todo-input>
+    <todo-lists></todo-lists>
 </div>
 </template>
 
 <script>
+import TodoHeader from "./TodoHeader.vue";
+import TodoLists from "./TodoLists.vue";
 import TodoInput from "./TodoInput.vue";
-
 export default {
     name: "TodoTemplate",
     components: {
+        TodoHeader,
+        TodoLists,
         TodoInput,
     },
 };
@@ -22,10 +26,11 @@ export default {
     height: 30rem;
     padding: 2rem;
     background: white;
-    margin: 100px auto;
+    margin: 200px auto;
     display: flex;
     justify-content: center;
     border-radius: 7%;
     border: 0.3px dotted lightslategrey;
+    flex-wrap: wrap;
 }
 </style>
