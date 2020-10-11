00986621d28947301a11244a3cc648c50e5c04d6
hackertaco <imsa1224@naver.com>
2020-10-12 02:18:37 +0900

    feat: ⚡️add header

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="header-block">
+    <div class="header-day">day</div>
+    <div class="header-todo">TODOLIST</div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "TodoHeader",
+};
+</script>
+
+<style>
+.header-block {
+    width: 100%;
+    height: 8%;
+    text-align: center;
+}
+
+.header-todo {
+    font-size: 1.7rem;
+    font-weight: bold;
+}
+
+.header-day {
+    text-align: right;
+    font-size: 0.3rem;
+}
+</style>
