f73b01b2f0e1bdfff77a2e9f8b8a4c3081687833
hackertaco <imsa1224@naver.com>
2020-10-12 02:18:56 +0900

    feat: ⚡️function addTodo

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,16 +1,39 @@
 <template>
-<input v-on="click" class="todo-input" />
+<input type="text" v-model="newTodoItem" @keyup.enter="addTodo" class="todo-input" />
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            newTodoItem: "",
+        };
+    },
     name: "TodoInput",
+
+    methods: {
+        addTodo() {
+            if (this.newTodoItem !== "") {
+                let obj = {
+                    completed: false,
+                    item: this.newTodoItem
+                };
+                localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
+                this.clearInput();
+            }
+        },
+        clearInput() {
+            this.newTodoItem = "";
+        },
+    },
 };
 </script>
 
 <style>
 .todo-input {
-    top: 25%;
-    position: absolute;
+    border: 1px dotted royalblue;
+    width: 80%;
+    height: 5%;
+    border-radius: 10px;
 }
 </style>
