78e0b1521b8fbf94c56167e987f975b304de00ce
hackertaco <imsa1224@naver.com>
2020-10-19 00:13:59 +0900

    feat: ⚡️get day

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,13 +1,21 @@
 <template>
 <div class="header-block">
-    <div class="header-day">day</div>
     <div class="header-todo">TODOLIST</div>
+    <div class="header-day">{{ day }}요일</div>
 </div>
 </template>
 
 <script>
 export default {
     name: "TodoHeader",
+    setup() {
+        const week = ["일", "월", "화", "수", "목", "금", "토"];
+        const today = new Date();
+        const day = week[today.getDay()];
+        return {
+            day,
+        };
+    },
 };
 </script>
 
@@ -16,6 +24,7 @@ export default {
     width: 100%;
     height: 8%;
     text-align: center;
+    margin-bottom: 1rem;
 }
 
 .header-todo {
@@ -24,7 +33,7 @@ export default {
 }
 
 .header-day {
-    text-align: right;
+    text-align: center;
     font-size: 0.3rem;
 }
 </style>
