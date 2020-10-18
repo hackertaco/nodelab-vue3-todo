75ab4066827e13b1d04cded5a56073cafc4e0783
hackertaco <imsa1224@naver.com>
2020-10-19 00:14:22 +0900

    feat: ⚡️remove & toggle

--- src/components/TodoLists.vue
+++ src/components/TodoLists.vue
@@ -1,35 +1,40 @@
 <template>
 <div class="lists-block">
     <ul>
-        <li v-for="todoItem in todoItems" :key="todoItem.item">
-            <span>{{ todoItem.item }}</span>
+        <li v-for="(todoItem, index) in this.todoItems" :key="todoItem.item">
+            <i class="fas fa-check checkBtn" @click="toggleComplete({ todoItem, index })" :class="{ checkBtnCompleted: todoItem.completed }"></i>
+            <span @click="toggleComplete({ todoItem, index })" :class="{ textCompleted: todoItem.completed }">{{ todoItem.item }}</span>
+            <span @click="removeTodo({ todoItem, index })" class="removeBtn"><i class="fas fa-trash"></i></span>
         </li>
     </ul>
 </div>
 </template>
 
 <script>
+import {
+    computed
+} from "vue";
+import {
+    mapMutations,
+    useStore
+} from "vuex";
 export default {
-    data: function () {
-        return {
-            todoItems: [],
-        };
+    methods: {
+        ...mapMutations({
+            removeTodo: "removeOneItem",
+            toggleComplete: "toggleOneItem",
+        }),
     },
-    methods: {},
-    mounted: function () {
-        if (localStorage.length !== 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                if (
-                    localStorage.key(i) !== "loglevel:webpack-dev-server" &&
-                    localStorage.key(i) !== "csCursors"
-                ) {
-                    console.log(localStorage.key(i));
-                    this.todoItems.push(
-                        JSON.parse(localStorage.getItem(localStorage.key(i)))
-                    );
-                }
-            }
-        }
+
+    setup() {
+        const store = useStore();
+
+        const todoItems = computed(() => {
+            return store.getters.getTodoItems;
+        });
+        return {
+            todoItems,
+        };
     },
 };
 </script>
@@ -40,5 +45,40 @@ export default {
     width: 100%;
     height: 70%;
     display: flex;
+    position: absolute;
+}
+
+li {
+    cursor: pointer;
+}
+
+.checkBtn {
+    line-height: 45px;
+    color: #f8c471;
+    margin-right: 10px;
+}
+
+.checkBtn:hover {
+    cursor: pointer;
+    color: #f8f9f9;
+}
+
+.checkBtnCompleted {
+    color: #b3adad;
+}
+
+.textCompleted {
+    text-decoration: line-through;
+    color: #b3adad;
+}
+
+.removeBtn {
+    margin-left: 10px;
+    color: #f8c471;
+}
+
+.removeBtn:hover {
+    color: #f8f9f9;
+    cursor: pointer;
 }
 </style>
