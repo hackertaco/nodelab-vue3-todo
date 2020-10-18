4abfbc41abce24ab1aed1ff2689b3966be42c719
hackertaco <imsa1224@naver.com>
2020-10-18 22:59:23 +0900

    feat: ⚡️clear all

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
+<span class="clearAll" @click="clearTodo">⚡️</span>
 <todo-template />
+<todo-input />
+<todo-lists />
 </template>
 
 <script>
 import TodoTemplate from "./components/TodoTemplate.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoLists from "./components/TodoLists.vue";
 export default {
     name: "App",
     components: {
         TodoTemplate,
+        TodoLists,
+        TodoInput,
+    },
+    methods: {
+        clearTodo() {
+            localStorage.clear();
+        },
     },
 };
 </script>
 
 <style>
+.clearAll {
+    padding: 5px;
+    color: black;
+    font-size: 4rem;
+}
+
 html {
     width: 100%;
     height: 100vh;
