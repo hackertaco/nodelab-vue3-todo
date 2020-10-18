2b51e9cb3146ef354affb435973c957844a6eb72
hackertaco <imsa1224@naver.com>
2020-10-18 23:00:17 +0900

    feat: ⚡️apply vue3 todo input

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,55 +1,35 @@
 <template>
 <input type="text" v-model="newTodoItem" @keyup.enter="addTodo" class="todo-input" />
-<div class="lists-block">
-    <ul>
-        <li v-for="(todoItem, index) in todoItems" :key="index">
-            <span class="todoItem-content">{{ todoItem.item }}</span>
-        </li>
-    </ul>
-</div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {
-            newTodoItem: "",
-            todoItems: [],
-            id: 1,
-        };
-    },
-    name: "TodoInput",
-    created: function () {
-        if (localStorage.length !== 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                if (
-                    localStorage.key(i) !== "loglevel:webpack-dev-server" &&
-                    localStorage.key(i) !== "csCursors"
-                ) {
-                    this.todoItems.unshift(
-                        JSON.parse(localStorage.getItem(localStorage.key(i)))
-                    );
-                }
-            }
-        }
-    },
-    methods: {
-        addTodo() {
-            if (this.newTodoItem !== "") {
-                let obj = {
-                    id: this.id++,
-                    completed: "false",
-                    item: this.newTodoItem,
-                };
-                this.todoItems.push(obj);
-                localStorage.setItem(this.newTodoItem, JSON.stringify(obj));
-                this.clearInput();
-            }
-        },
+import {
+    ref
+} from "vue";
+import {
+    useStore
+} from "vuex";
 
-        clearInput() {
-            this.newTodoItem = "";
-        },
+export default {
+    name: "TodoInput",
+    setup() {
+        const store = useStore();
+        const newTodoItem = ref("");
+
+        const addTodo = () => {
+            if (newTodoItem.value !== "") {
+                const text = newTodoItem.value.trim();
+                store.commit("addOneItem", text);
+                clearInput();
+            }
+        };
+        const clearInput = () => {
+            newTodoItem.value = "";
+        };
+        return {
+            addTodo,
+            newTodoItem,
+        };
     },
 };
 </script>
@@ -57,9 +37,11 @@ export default {
 <style>
 .todo-input {
     border: 1px dotted royalblue;
-    width: 80%;
-    height: 5%;
+    width: 10%;
+    height: 3%;
     border-radius: 10px;
+
+    top: 30%;
 }
 
 .lists-block {
