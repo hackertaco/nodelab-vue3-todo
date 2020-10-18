6e0b8d055b99cfdc39a5edc31f80e0b9fb486778
hackertaco <imsa1224@naver.com>
2020-10-19 00:13:44 +0900

    style: 💅 todo list box

--- src/components/TodoTemplate.vue
+++ src/components/TodoTemplate.vue
@@ -1,33 +1,24 @@
 <template>
 <div class="todo-background">
     <todo-header></todo-header>
-    <todo-input></todo-input>
 </div>
 </template>
 
 <script>
 import TodoHeader from "./TodoHeader.vue";
-import TodoInput from "./TodoInput.vue";
 export default {
     name: "TodoTemplate",
     components: {
         TodoHeader,
-        TodoInput,
     },
 };
 </script>
 
 <style scoped>
 .todo-background {
-    width: 20rem;
-    height: 30rem;
-    padding: 2rem;
-    background: white;
-    margin: 200px auto;
     display: flex;
     justify-content: center;
     border-radius: 7%;
-    border: 0.3px dotted lightslategrey;
     flex-wrap: wrap;
 }
 </style>
